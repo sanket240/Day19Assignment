d6179b45ad86536175aaf1189302687b66eeebeb
sanket2498 <sanketdulange24@gmail.com>
2020-11-12 14:22:07 +0530

    Added : Part Time Employee and Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,7 +7,8 @@ public class EmployeeWageComputation
 		int FULL_DAY_HOUR=8;
 		int dailyWage=0;
 		int employee_hours=0;
-		int IS_FULL_TIME=1;
+		int PART_TIME_HOUR=8;
+		double partTime=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
 			System.out.println("Employee is present");
@@ -15,5 +16,8 @@ public class EmployeeWageComputation
 			System.out.println("Employee is Absent");
 		dailyWage=(employee_hours*WAGE_PER_HOUR);
 		System.out.println("Daily Wage:"+dailyWage);
+		double partTime_hours=Math.floor(Math.random()*10)%8;
+		partTime=(partTime_hours*WAGE_PER_HOUR);
+		System.out.println("Part time wages:"+partTime);
 	}
 }
