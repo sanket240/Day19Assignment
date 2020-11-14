3098033fdc65b3586872f191d87ba506a0378094
sanket2498 <sanketdulange24@gmail.com>
2020-11-14 14:33:19 +0530

    Added : Total Wage for Each Company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,92 +1,62 @@
-package EmpWage;
-import java.util.Scanner;
-public class EmployeeWageComputation
+public class  EmployeeWageComputation
 {
-        public static void Refactor(String name,int Workingdays,int Workinghours)
-        {
-                int IS_FULL_TIME=1;
-                int WAGE_PER_HOUR=20;
-                int FULL_DAY_HOUR=8;
-                int WORKING_DAYS=Workingdays;
-                int dailyWage=0;
-                int employee_hours=0;
-                int PART_TIME_HOUR=8;
-                double partTime=0;
-                int monthlyWage=0;
-                int total_Monthly_Wage=0;
-                int days=0;
-                int hours=0;
-                int empHr=0;
-                String n1=name;
-                System.out.println("Name is:"+n1);
-                double empCheck=Math.floor(Math.random()*10)%2;
-                System.out.println("!.Employee Attendance 2.DailyWages 3.Part Time Wages Enter your choice:");
-                Scanner s1=new Scanner(System.in);
-                int choice=s1.nextInt();
-                switch(choice)
+	int IS_FULL_TIME=1;
+    	int WAGE_PER_HOUR=20;
+    	int FULL_DAY_HOUR=8;
+        int dailyWage=0;
+        int employee_hours=0;
+        int PART_TIME_HOUR=8;
+        double partTime=0;
+        int monthlyWage=0;
+        int total_Monthly_Wage=0;
+        int days=0;
+        int hours=0;
+        int empHr=0;
+        String name;
+        int Workingdays;
+        int Workinghours;
+    	public  EmployeeWageComputation(String name,int Workingdays,int Workinghours)
+    	{
+        	this.name=name;
+        	this.Workingdays=Workingdays;
+        	this.Workinghours=Workinghours;
+    	}
+	public void EmployeeMonthlyWage()
+    	{
+        	while(days<=Workingdays && empHr<=Workinghours)
                 {
-                case 1:
-                        if(empCheck==IS_FULL_TIME)
-                        {
-                                System.out.println("Employee is present");
-                                employee_hours=8;
-                        }
-                        else
-                        {
-                                System.out.println("Employee is Absent");
-                                employee_hours=0;
-                        }
-                        break;
-                case 2:
-                        employee_hours=8;
-                        dailyWage=(employee_hours*WAGE_PER_HOUR);
-                        System.out.println("Daily Wage:"+dailyWage);
-                        break;
-                case 3:
-                        double partTime_hours=Math.floor(Math.random()*10)%8;
-                        partTime=(partTime_hours*WAGE_PER_HOUR);
-                        System.out.println("Part time wages:"+partTime);
-                        break;
-                default:
-                        System.out.println("Wrong choice");
-                }
-                dailyWage=(employee_hours*WAGE_PER_HOUR);
-                monthlyWage=(WORKING_DAYS*dailyWage);
-                System.out.println("Monthly Wages:"+monthlyWage);
-                System.out.println("DailyWage: MonthlWage:");
-                while(days<=Workingdays && empHr<=Workinghours)
-                {
-                        double empCheck1=Math.floor(Math.random()*10)%1;
+                	double empCheck1=Math.floor(Math.random()*10)%1;
                         int empCheck2=(int)empCheck1;
                         switch(empCheck2)
                         {
-                        case 0:
-                                empHr=empHr+4;
-                                break;
-                        case 1:
-                                empHr=empHr+8;
-                                break;
-                        default:
-                                System.out.println("Wrong choice");
+                        	case 0:
+                                	empHr=empHr+4;
+                                	break;
+                        	case 1:
+                                	empHr=empHr+8;
+                                	break;
+                        	default:
+                                	System.out.println("Wrong choice");
                         }
                         days++;
                         dailyWage=(empHr*WAGE_PER_HOUR);
-                        total_Monthly_Wage=(total_Monthly_Wage+dailyWage);
-                        System.out.println(dailyWage + " " + total_Monthly_Wage);
                 }
-        }
-        
-        public static void main(String args[])
-        {
-        	System.out.println("Start");
-        	Scanner s1=new Scanner(System.in);
-        	System.out.println("Enter company name:");
-        	String company=s1.next();
-        	System.out.println("Enter Working Days:");
-        	int days=s1.nextInt();
-        	System.out.println("Enter Working Hours:");
-        	int hrs=s1.nextInt();
-        	Refactor(company,days,hrs);
-        }
-        
+                total_Monthly_Wage=Workinghours*WAGE_PER_HOUR;
+    	}
+    	public String toString()
+    	{
+        	return "Total Employee Wage for company is:"+name+"is:"+total_Monthly_Wage;
+    	}
+	public static void main(String args[])
+	{
+     		EmployeeWageComputation emp=new EmployeeWageComputation("Atos",20,500);
+     		EmployeeWageComputation emp1=new EmployeeWageComputation("Silverlink",20,450);
+     		EmployeeWageComputation emp2=new EmployeeWageComputation("TCS",20,600);
+     		emp.EmployeeMonthlyWage();
+     		System.out.println(emp);
+     		emp1.EmployeeMonthlyWage();
+     		System.out.println(emp1);
+     		emp2.EmployeeMonthlyWage();
+     		System.out.println(emp2);
+    	}
 }
